13d82826acc670080809f4d1a83336d2d3653f06
jitengjiao <jitengjiao@bytedance.com>
2023-01-20 11:24:18 +0800

    feat: add new project

--- TestProject/app/src/main/java/com/jiyoung/testproject/MainActivity.kt
+++ TestProject/app/src/main/java/com/jiyoung/testproject/MainActivity.kt
@@ -3,11 +3,21 @@ package com.jiyoung.testproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "哈哈哈哈")
+
+        bindUI()
+    }
+
+    fun bindUI() {
+        val button1: Button = findViewById(R.id.button0)
+        button1.setOnClickListener {
+            Toast.makeText(this, "You clciked Button 0", Toast.LENGTH_SHORT).show()
+        }
     }
 }
