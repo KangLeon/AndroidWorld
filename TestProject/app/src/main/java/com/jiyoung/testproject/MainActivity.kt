94c352b6c6ef905694b7221cdd5b574f45ce383e
jitengjiao <jitengjiao@bytedance.com>
2023-01-20 12:28:10 +0800

    feat: add new ui

--- TestProject/app/src/main/java/com/jiyoung/testproject/MainActivity.kt
+++ TestProject/app/src/main/java/com/jiyoung/testproject/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.jiyoung.testproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +17,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun bindUI() {
-        val button1: Button = findViewById(R.id.button0)
-        button1.setOnClickListener {
-            Toast.makeText(this, "You clciked Button 0", Toast.LENGTH_SHORT).show()
+        button0.setOnClickListener {
+            //显示intent
+//            var intent = Intent(this, FirstActivity::class.java)
+//            startActivity(intent)
+            //隐式intent
+            var intent = Intent("com.jiyoung.testobject.ACTION_START")
+            startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "添加基金", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "编辑基金", Toast.LENGTH_SHORT).show()
+        }
+
+        return  true
     }
 }
