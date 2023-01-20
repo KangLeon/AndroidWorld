c039b60b4c229ce79e09ba3f1c65a55552eb4cd0
jitengjiao <jitengjiao@bytedance.com>
2023-01-20 14:09:11 +0800

    feat: add new ui

--- TestProject/app/src/main/java/com/jiyoung/testproject/FirstActivity.kt
+++ TestProject/app/src/main/java/com/jiyoung/testproject/FirstActivity.kt
@@ -1,11 +1,29 @@
 package com.jiyoung.testproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.first_layout.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_layout)
+
+        bindUI()
+    }
+
+    fun bindUI() {
+        var extraData = intent.getStringExtra("fund_id")
+
+        Toast.makeText(this,"传入了数据，${extraData}", Toast.LENGTH_SHORT).show()
+
+        button1.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("data_return", "Hello FirstActivity")
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
--- TestProject/app/src/main/java/com/jiyoung/testproject/MainActivity.kt
+++ TestProject/app/src/main/java/com/jiyoung/testproject/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.jiyoung.testproject
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,14 +21,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun bindUI() {
+        val startActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode === Activity.RESULT_OK) {
+                val returnedData = it.data?.getStringExtra("data_return")
+                Toast.makeText(this,"返回了数据，${returnedData}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         button0.setOnClickListener {
             //显示intent
 //            var intent = Intent(this, FirstActivity::class.java)
 //            startActivity(intent)
+
             //隐式intent
-            var intent = Intent("com.jiyoung.testobject.ACTION_START")
-            startActivity(intent)
+            var fundId = "001404"
+            var intent = Intent(this, FirstActivity::class.java)
+            intent.putExtra("fund_id", fundId)
+            startActivity.launch(intent)
         }
+    }
+
+    fun openURL() {
+        var intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse("http://jiyoung.cn")
+        startActivity(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -36,6 +56,7 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.add_item -> Toast.makeText(this, "添加基金", Toast.LENGTH_SHORT).show()
             R.id.remove_item -> Toast.makeText(this, "编辑基金", Toast.LENGTH_SHORT).show()
+            R.id.about_fund -> openURL()
         }
 
         return  true
