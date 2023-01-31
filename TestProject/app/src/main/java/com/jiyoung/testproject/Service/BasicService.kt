b72425bf0807044453a2bf0395b6b3422e349df3
jitengjiao <jitengjiao@bytedance.com>
2023-01-31 20:31:43 +0800

    feat: add service

--- TestProject/app/src/main/java/com/jiyoung/testproject/Service/BasicService.kt
+++ TestProject/app/src/main/java/com/jiyoung/testproject/Service/BasicService.kt
@@ -13,9 +13,15 @@ class BasicService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        
+        Log.d("BasicService","onStartCommand executed")
         return super.onStartCommand(intent, flags, startId)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MyService","onDestroy executed")
+    }
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
